36afddb740366c588bba492cbe0a8d4997f4d706
wjb127 <wjb127@naver.com>
2024-10-13 14:57:35 +0900

    tailwind 스타일 적용

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,8 +1,10 @@
+// tailwind.config.js
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
-    './app/**/*.{js,ts,jsx,tsx}', // Next.js 13의 app directory를 사용할 경우
+    './pages/**/*.{js,ts,jsx,tsx}',   // pages 디렉토리 내의 모든 JS, TS, JSX, TSX 파일
+    './components/**/*.{js,ts,jsx,tsx}',  // components 디렉토리 내의 파일들
+    './app/**/*.{js,ts,jsx,tsx}',     // Next.js 13 이상일 경우 app 디렉토리 사용
+    './src/**/*.{js,ts,jsx,tsx}',     // src 디렉토리를 사용하는 경우
   ],
   theme: {
     extend: {},
